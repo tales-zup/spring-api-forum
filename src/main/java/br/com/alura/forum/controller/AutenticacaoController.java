e68d855ee6aa7eb86008fbb5b8a9fe35fa50014c
tales-zup <tales.araujo@zup.com.br>
2021-08-18 20:47:17 -0300

    retornando token no endpoint auth

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,6 +1,7 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +33,7 @@ public class AutenticacaoController {
             Authentication authentication = authenticationManager.authenticate(login);
             String token = tokenService.gerarToken(authentication);
             System.out.println(token);
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
         } catch (AuthenticationException e) {
             return ResponseEntity.badRequest().build();
         }
