759157dd106d14171d18463d173a710532c26e45
tales-zup <tales.araujo@zup.com.br>
2021-08-18 11:26:20 -0300

    configurando urls para security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +12,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    // Configs de autenticacao
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
 
+    // Configs de autorizacao
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    // Configs de recursos estaticos(css, js, imgs..)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
 }
