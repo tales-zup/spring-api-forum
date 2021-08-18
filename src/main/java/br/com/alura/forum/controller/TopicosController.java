cad5c557dc08de472d25b428fb4beae24d8c8346
tales-zup <tales.araujo@zup.com.br>
2021-08-18 09:39:48 -0300

    recebendo pageable do request

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,9 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -35,11 +35,7 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-								 @RequestParam int pagina,
-								 @RequestParam int qtd,
-								 @RequestParam String ordenacao) {
-
-		Pageable pageable = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
+								 @PageableDefault(sort="id", direction = Direction.DESC) Pageable pageable) {
 
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(pageable);
