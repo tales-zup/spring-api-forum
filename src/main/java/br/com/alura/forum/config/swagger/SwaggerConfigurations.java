423e6c094ce892cc517e396ed9f82d5067145fd0
tales-zup <tales.araujo@zup.com.br>
2021-08-19 09:03:23 -0300

    repassando anotacao swagger2 para classe main

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -9,12 +9,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfigurations {
 
     @Bean
