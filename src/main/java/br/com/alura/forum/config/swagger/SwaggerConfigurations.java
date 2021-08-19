461b2292f243fa9b93e3d26c2b30089a4a3303af
tales-zup <tales.araujo@zup.com.br>
2021-08-19 09:00:54 -0300

    add header authorization swagger

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -3,11 +3,15 @@ package br.com.alura.forum.config.swagger;
 import br.com.alura.forum.modelo.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +24,16 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()
+                ));
     }
 
 }
