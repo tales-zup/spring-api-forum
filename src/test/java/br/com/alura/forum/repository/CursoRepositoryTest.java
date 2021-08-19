609bc5f938e5d65b6097dacf5e195edcbf06cc58
tales-zup <tales.araujo@zup.com.br>
2021-08-19 14:15:07 -0300

    teste com profile test

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,19 +5,33 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+// A anotação abaixo diz para o Spring não substituir o banco de dados configurado pelo banco em memória.
+// O comportamento padrão é que para testes o Spring sempre usa um banco em memória.
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test") // Vai forçar que o profile seja "test"
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
         String nomeCurso = "HTML 5";
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Porogramacao");
+        em.persist(html5);
         Curso curso = cursoRepository.findByNome(nomeCurso);
         Assert.assertNotNull(curso);
         Assert.assertEquals(nomeCurso, curso.getNome());
